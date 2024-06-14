bb1de208a4f3d9420b23a087024ce379f63973f4
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2024-06-14 09:33:29 +0800

    Update twrp_PD1936.mk

--- twrp_PD1936.mk
+++ twrp_PD1936.mk
@@ -1,29 +1,24 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 #
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from PD1936 device
-$(call inherit-product, device/vivo/PD1936/device.mk)
 
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := PD1936
 PRODUCT_NAME := twrp_PD1936
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := V1936A
 PRODUCT_MANUFACTURER := vivo
-
-PRODUCT_GMS_CLIENTID_BASE := android-vivo
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="PD1936-user 11 RP1A.200720.012 compiler1115194718 release-keys"
-
-BUILD_FINGERPRINT := vivo/PD1936/PD1936:11/RP1A.200720.012/compiler1115194718:user/release-keys
